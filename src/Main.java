cb83bd2b5dd692d62bfa1f8c24567bc06d568b4d
Askomm <azamat.kuanyshpayev@gmail.com>
2025-12-17 12:28:31 +0500

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main{
-    public static void main(String[] args){
-        System.out.println("Hello world");
+    public static void area(double width, double height){
+        return width*height;
+    }
+    public static void perimetr(double width, double height){
+        return (width+height)*2;
+    }
+    public static void Rectangle(double width, double height){
+        boolean check = false;
+        if(width >1 && height >1 ){
+            check = true;
+        }
+        else{
+            check = false;
+        }
+    }
+    private static void field(){
+        double width = 1.0;
+        double height = 1.0 ;
+        int id;
+        final int idGen;
+    }
+    public static void main(){
+        Rectangle(field.width(),field.height());
+        perimetr(field.width(),field.height());
+        area(field.width(),field.height());
     }
 }
